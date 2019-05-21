d5867f735c85e3be8cc817d65b43953a24d790e6
Kenta Tohashi <KentaTohashi@gmail.com>
2019-05-21 17:55:14 +0900

    QRコードの文字列を読みやすくした

--- NDI-Video-Receiver/NDI-Video-Receiver/NdiCom.cpp
+++ NDI-Video-Receiver/NDI-Video-Receiver/NdiCom.cpp
@@ -334,19 +334,20 @@ void NdiCom::writeDecodeResult(cv::Mat &im, vector<decodedObject>&decodedObjects
          data, // 文字列
          point, // 座標
          cv::FONT_HERSHEY_SIMPLEX, // フォントの種類
-         0.8, // 文字の大きさ
-         cv::Scalar(255, 255, 255), // 文字の色
-         3 // 線の太さ
+         1.2, // 文字の大きさ
+         cv::Scalar(255, 255, 0), // 文字の色
+         10, // 線の太さ
+         cv::LINE_AA
      );
      cv::putText( // インラインフォント
          im, // 画像
          data, // 文字列
          point, // 座標
          cv::FONT_HERSHEY_SIMPLEX, // フォントの種類
-         0.8, // 文字の大きさ
+         1.2, // 文字の大きさ
          cv::Scalar(0, 0, 0), // 文字の色
-         1, // 線の太さ
-         CV_AA // アンチエイリアス
+         3, // 線の太さ
+         cv::LINE_AA // アンチエイリアス
      );
    }
    
